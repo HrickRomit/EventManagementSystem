cc9f0e2f0ad8a1cdce472fe4d5644b112f6105ff
HrickRomit <hrickromit@gmail.com>
2026-05-16 13:36:56 +0600

    some more deployment issues

--- server/src/config/firebaseAdmin.js
+++ server/src/config/firebaseAdmin.js
@@ -1,15 +1,19 @@
 import admin from "firebase-admin";
+import { readFileSync } from "node:fs";
 
 const getFirebaseAdmin = () => {
   if (admin.apps.length) {
     return admin;
   }
 
-  const projectId = process.env.FIREBASE_PROJECT_ID || "event-management-7c019";
+  const projectId = process.env.FIREBASE_PROJECT_ID || "sems-9d72b";
   const serviceAccountJson = process.env.FIREBASE_SERVICE_ACCOUNT;
+  const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
 
-  if (serviceAccountJson) {
-    const serviceAccount = JSON.parse(serviceAccountJson);
+  if (serviceAccountJson || serviceAccountPath) {
+    const serviceAccount = serviceAccountJson
+      ? JSON.parse(serviceAccountJson)
+      : JSON.parse(readFileSync(serviceAccountPath, "utf8"));
 
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
